436b7ffba1e4aff0a5a09defa4fc183b54210b36
Temizer <tamer.temizer@siemens.com>
2017-06-08 15:50:00 +0300

    Restored

--- main.cpp
+++ main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
-	cout << "IT'S ALIVE!";
+	// Infinite loop
+	while (true) 
+	{
+		// Dynamically allocate memory but don't delete it.
+		int* x = new int[1];
+		x[0] = 5;
+		cout << "IT'S ALIVE! " << x[0]<<endl;
+
+		// Unreachable piece of code
+		if (false)
+		{
+			cout << "\nImpossible condition";
+		}
+	}
+	
 	return 0;
 }
